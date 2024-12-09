475279007fc3a2a39f6eee16de6be136e5e62a47
Amar Patra <amarpatra932@gmail.com>
2024-12-09 21:20:46 +0530

    projects part complete

--- src/containers/project/index.jsx
+++ src/containers/project/index.jsx
@@ -1,4 +1,7 @@
 import styles from "./portfolio.module.css";
+import projects from "../../components/data/projects/projects.json";
+import { Link } from "react-router-dom";
+
 const Project = () => {
   return (
     <>
@@ -6,8 +9,47 @@ const Project = () => {
         <div className={styles.heading}>
           <h1>Projects</h1>
         </div>
-        <div className={styles.containerProjects}>
+        <div className={`row  ${styles.containerProjects}`}>
+          {projects.map((projects) => {
+            return (
+              <>
+                <div key={projects.id} className="my-4 col-md-3 mx-4">
+                  <div className={styles.cardBox} style={{ width: "18rem" }}>
+                    <div className={styles.img}>
+                      <img
+                        src={`projects/${projects.imgSrc}`}
+                        className={`card-img-top ${styles.cardImgTop}`}
+                        alt="Loading"
+                      />
+                    </div>
 
+                    <div className={`card-body ${styles.cardBody}`}>
+                      <h2 className={`card-title ${styles.cardTitle}`}>
+                        {projects.title}
+                      </h2>
+                      <p className={`card-text my-1 ${styles.cardText}`}>
+                        {projects.about}
+                      </p>
+                      <div className="btn">
+                        <Link
+                          to={projects.link}
+                          className={`btn btn-info mx-2 ${styles.btnPrimary}`}
+                        >
+                          Go & Check
+                        </Link>
+                        <Link
+                          to={projects.code}
+                          className={`btn btn-warning mx-2 ${styles.btnPrimary}`}
+                        >
+                          Code
+                        </Link>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </>
+            );
+          })}
         </div>
       </div>
     </>
