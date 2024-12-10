9699479a07f5e62d6b8af22700c14f0643e95740
Amar Patra <amarpatra932@gmail.com>
2024-12-10 15:21:55 +0530

    about part complete and complete all 1

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -7,68 +7,70 @@ const NavBar = () => {
       <div
         className={`container ${styles.navbar} `}
         data-aos="fade-down-left"
-        data-aos-duration="2000"
+        data-aos-duration="800"
       >
         <div
           className={`${styles.leftNav} ${styles.navItems}`}
+          data-aos="fade-up"
+          data-aos-duration="1000"
         >
-          Portfolio
+          Amar Patra
         </div>
         <div className={styles.rightNav}>
           <Link
             className={styles.navItems}
             to="/"
             data-aos="fade-up"
-            data-aos-duration="3000"
+            data-aos-duration="1000"
           >
             Home
           </Link>
           <Link
             className={styles.navItems}
-            to="/project"
+            to="/about"
             data-aos="fade-down"
-            data-aos-duration="3000"
+            data-aos-duration="1000"
+          >
+            About
+          </Link>
+          <Link
+            className={styles.navItems}
+            to="/project"
+            data-aos="fade-up"
+            data-aos-duration="1000"
           >
             Projects
           </Link>
           <Link
             className={styles.navItems}
             to="/skills"
-            data-aos="fade-up"
-            data-aos-duration="3000"
+            data-aos="fade-down"
+            data-aos-duration="1000"
           >
             Skills
           </Link>
           <Link
             className={styles.navItems}
-            to="/resume"
-            data-aos="fade-down"
-            data-aos-duration="3000"
+            to="/experience"
+            data-aos="fade-up"
+            data-aos-duration="1000"
           >
-            Resume
+            Experience
           </Link>
           <Link
             className={styles.navItems}
-            to="/experience"
-            data-aos="fade-up"
-            data-aos-duration="3000"
+            to="/resume"
+            data-aos="fade-down"
+            data-aos-duration="1000"
           >
-            Experience
+            Resume
           </Link>
 
           <Link
             className={styles.navItems}
-            to="/about"
-            data-aos="fade-down"
-            data-aos-duration="3000"
-          >
-            About
-          </Link>
-          <Link
-            className={styles.navItems}
             to="/contact"
             data-aos="fade-up"
-            data-aos-duration="3000"
+            data-aos-duration="1000"
           >
             Contact
           </Link>
