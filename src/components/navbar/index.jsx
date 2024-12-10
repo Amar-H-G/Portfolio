bfbd7284a927653f829f8e2e4ef7c9d6dca1ffe1
Amar Patra <amarpatra932@gmail.com>
2024-12-10 14:57:50 +0530

    about part complete and complete all

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -4,29 +4,73 @@ import styles from "./Nav.module.css";
 const NavBar = () => {
   return (
     <>
-      <div className={`container ${styles.navbar} `} data-aos="fade-down-left" data-aos-duration="2000" >
-        <div className={`${styles.leftNav} ${styles.navItems}`}>Portfolio</div>
+      <div
+        className={`container ${styles.navbar} `}
+        data-aos="fade-down-left"
+        data-aos-duration="2000"
+      >
+        <div
+          className={`${styles.leftNav} ${styles.navItems}`}
+        >
+          Portfolio
+        </div>
         <div className={styles.rightNav}>
-          <Link className={styles.navItems} to="/">
+          <Link
+            className={styles.navItems}
+            to="/"
+            data-aos="fade-up"
+            data-aos-duration="3000"
+          >
             Home
           </Link>
-          <Link className={styles.navItems} to="/project">
+          <Link
+            className={styles.navItems}
+            to="/project"
+            data-aos="fade-down"
+            data-aos-duration="3000"
+          >
             Projects
           </Link>
-          <Link className={styles.navItems} to="/skills">
+          <Link
+            className={styles.navItems}
+            to="/skills"
+            data-aos="fade-up"
+            data-aos-duration="3000"
+          >
             Skills
           </Link>
-          <Link className={styles.navItems} to="/resume">
+          <Link
+            className={styles.navItems}
+            to="/resume"
+            data-aos="fade-down"
+            data-aos-duration="3000"
+          >
             Resume
           </Link>
-          <Link className={styles.navItems} to="/experience">
+          <Link
+            className={styles.navItems}
+            to="/experience"
+            data-aos="fade-up"
+            data-aos-duration="3000"
+          >
             Experience
           </Link>
-          <Link className={styles.navItems} to="/contact">
-            Contact
-          </Link>
-          <Link className={styles.navItems} to="/about">
+
+          <Link
+            className={styles.navItems}
+            to="/about"
+            data-aos="fade-down"
+            data-aos-duration="3000"
+          >
             About
+          </Link>
+          <Link
+            className={styles.navItems}
+            to="/contact"
+            data-aos="fade-up"
+            data-aos-duration="3000"
+          >
+            Contact
           </Link>
         </div>
       </div>
--- src/containers/about/index.jsx
+++ src/containers/about/index.jsx
@@ -1,6 +1,62 @@
-import './about.modules.css'
+import styles from "./about.module.css";
 
-const About = ()=>{
-  return <h1>Hey This About Me</h1>
-}
+const About = () => {
+  return (
+    <>
+      <div className={styles.container}>
+        <div
+          className={styles.heading}
+          data-aos="fade-down-right"
+          data-aos-duration="3000"
+        >
+          <h1 className={styles.head}>About</h1>
+        </div>
+        <div className={`${styles.body} `}>
+          <div
+            className={styles.text1}
+            data-aos="flip-up"
+            data-aos-duration="3000"
+          >
+            I am a passionate and driven web developer with a keen interest in
+            building dynamic and user-friendly web applications. My journey into
+            development is fueled by my love for technology and problem-solving,
+            which inspires me to create solutions that simplify and enhance
+            everyday experiences.
+          </div>
+          <div
+            className={styles.text2}
+            data-aos="flip-up"
+            data-aos-duration="3000"
+          >
+            <p>
+              I specialize in front-end and full-stack development, leveraging
+              technologies like HTML, CSS, JavaScript, Python, and Django to
+              build responsive and scalable projects. From crafting engaging
+              designs to implementing robust back-end systems, I focus on
+              delivering impactful, functional, and aesthetically pleasing
+              applications.
+            </p>
+          </div>
+          <div
+            className={styles.text3}
+            data-aos="flip-up"
+            data-aos-duration="3000"
+          >
+            <p>
+              Beyond coding, I am committed to continuous learning, currently
+              enhancing my skills in data structures and algorithms to improve
+              my problem-solving abilities. My dedication to personal growth and
+              innovation drives me to stay updated with the latest industry
+              trends and best practices. Whether collaborating in teams or
+              working independently, I thrive on turning ideas into reality and
+              contributing to meaningful projects that leave a lasting
+              impression. My portfolio reflects my journey, showcasing projects
+              that embody creativity, dedication, and technical expertise.
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 export default About;
